0c5b5133c3c094a4f8f744967825919b193fca1d
Norbert Palinkas <norbert@palinkasnorbert.hu>
2024-02-01 18:18:54 +0100

    updated staircase code

--- create_staircase_lists.py
+++ create_staircase_lists.py
@@ -1,18 +1,4 @@
 def create_staircase(nums):
-    while len(nums) != 0:
-        step = 1
-        subsets = []
-        if len(nums) >= step:
-            subsets.append(nums[0:step])
-            nums = nums[step:]
-            step += 1
-        else:
-            return False
-
-    return subsets
-
-
-def create_staircase2(nums):
     step = 1
     subsets = []
     while len(nums) != 0:
@@ -33,6 +19,3 @@ print(nums[0])
 print(nums[1])
 subsets.append(nums[0:step])
 print(subsets)
-
-# print(create_staircase(nums))
-# print(create_staircase2(nums))
